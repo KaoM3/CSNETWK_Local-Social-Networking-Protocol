ea2209a7512993ded719ed8e901884f54dc8b4a1
KaoM3 <152694740+KaoM3@users.noreply.github.com>
2025-07-13 12:33:18 +0800

    feat(unicast): basic socket setup\

--- client.py
+++ client.py
@@ -1,9 +1,38 @@
+import socket
+import config
+import threading
+import argparse
+
 # Is the entry point of the application.
 # Handles user input as well
 
 # TODO: Add config initialization
 # TODO: Create broadcasting socket
 # TODO: Create unicast socket
-# TODO: Implement listening for incoming messages
+
+def main():
+  # PORT AND CLIENT IP
+  parser = argparse.ArgumentParser()
+  parser.add_argument("port", type=int, help="Port number to use")
+  args = parser.parse_args()
+  print("Using port:", args.port)
+  print(config.CLIENT_IP)
+
+  unicast_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+  # For LOCAL MACHINE TESTING use args
+  unicast_socket.bind((config.CLIENT_IP, args.port))#config.UNICAST_PORT))
+
+  # TODO: Implement listening for incoming messages
+  def unicast_receive_loop():
+    while True:
+      data, addr = unicast_socket.recvfrom(1024)
+      print(f"[RECV] {addr}: {data.decode()}")
+
+  threading.Thread(target=unicast_receive_loop, daemon=True).start()
+  input("Press Enter to exit...\n")
+
 # TODO: Implement passing incoming messages to route.py
 # TODO: Implement user interactions (console based)
+
+if __name__ == "__main__":
+  main()
--- config.py
+++ config.py
@@ -6,3 +6,18 @@
 # unicast port (50999)
 # buffer size for port receive (unknown as of now)
 # ttl default (if any)
+
+import socket
+
+def get_ip():
+  s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+  try:
+    # Doesn't send data, just uses the OS routing table
+    s.connect(("8.8.8.8", 80))
+    return s.getsockname()[0]
+  finally:
+    s.close()
+
+PING_INTERVAL = 300
+UNICAST_PORT = 50999
+CLIENT_IP = get_ip()

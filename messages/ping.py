71367a68953548d7ad28fab0a20516e8c846d226
KaoM3 <rafaeldcbernandino@gmail.com>
2025-07-19 20:39:32 +0800

    feat(ping): add ping message class

--- messages/ping.py
+++ messages/ping.py
@@ -1,3 +1,39 @@
-# TODO: Implement Schema
-# TODO: Implement send()
-# TODO: Implement receive()
+from custom_types.user_id import UserID
+from utils import format
+from messages.base_message import BaseMessage
+import socket
+
+class Ping(BaseMessage):
+
+  __schema__ = {
+    "TYPE": "PING",
+    "USER_ID": {"type": UserID, "required": True}
+  }
+
+  @property
+  def payload(self) -> dict:
+    return {
+      "TYPE": self.type,
+      "USER_ID": self.user_id
+    }
+
+  def __init__(self, user_id: UserID):
+    self.type = "PING"
+    self.user_id = user_id
+    format.validate_message(self.payload, self.__schema__)
+
+  @classmethod
+  def parse(cls, data: dict) -> "Ping":
+    return cls(
+      user_id=data["USER_ID"]
+    )
+        
+  @classmethod
+  def receive(cls, raw: str) -> "Ping":
+    return cls.parse(format.deserialize_message(raw))
+
+  def send(self, socket: socket.socket, ip: str, port: int, encoding: str="utf-8"):
+    msg = format.serialize_message(self.payload)
+    socket.sendto(msg.encode(encoding), (ip, port))
+
+__message__ = Ping

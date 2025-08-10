f3cb866a434536e79ba31e9a69e91ab33da96577
Reinman <reinmang@gmail.com>
2025-08-10 22:44:05 +0800

    fix(group): fix update schema

--- messages/group_update.py
+++ messages/group_update.py
@@ -3,10 +3,15 @@
 # TODO: Implement receive()
 
 from datetime import datetime, timezone
-from custom_types.fields import UserID, Token, Timestamp, MessageID, TTL
-from custom_types.base_message import BaseMessage
+import socket
+import config
+
 from states.client_state import client_state
+from custom_types.base_message import BaseMessage
+from custom_types.fields import UserID, Token, Timestamp, TTL
+from datetime import datetime, timezone
 from utils import msg_format
+from client_logger import client_logger
 
 class GroupUpdate(BaseMessage):
     TYPE = "GROUP_UPDATE"
@@ -15,22 +20,32 @@ class GroupUpdate(BaseMessage):
         "TYPE": TYPE,
         "FROM": {"type": UserID, "required": True},
         "GROUP_ID": {"type": str, "required": True},
-        "UPDATE_TYPE": {"type": str, "required": True},
-        "UPDATE_DATA": {"type": str, "required": False},
-        "MESSAGE_ID": {"type": MessageID, "required": True},
+        "ADD": {"type": str, "required": True},
+        "REMOVE": {"type": str, "required": False},
         "TIMESTAMP": {"type": Timestamp, "required": True},
         "TOKEN": {"type": Token, "required": True},
     }
 
-    def __init__(self, group_id: str, update_type: str, update_data: str = "", ttl: TTL = 3600):
+    @property
+    def payload(self) -> dict:
+        return {
+            "TYPE": self.TYPE,
+            "FROM": self.from_user,
+            "GROUP_ID": self.group_id,
+            "ADD": self.add,
+            "REMOVE": self.remove,
+            "TIMESTAMP": self.timestamp,
+            "TOKEN": self.token,
+        }
+    
+    def __init__(self, group_id: str, add: str, remove: str = "", ttl: TTL = 3600):
         unix_now = int(datetime.now(timezone.utc).timestamp())
         self.type = self.TYPE
         self.from_user = client_state.get_user_id()
         self.group_id = group_id
-        self.update_type = update_type
-        self.update_data = update_data
+        self.add = add
+        self.remove = remove
         self.timestamp = Timestamp(unix_now)
-        self.message_id = MessageID.generate()
         self.token = Token(self.from_user, self.timestamp + ttl, Token.Scope.GROUP)
 
     @classmethod
@@ -39,9 +54,8 @@ class GroupUpdate(BaseMessage):
         new_obj.type = data["TYPE"]
         new_obj.from_user = UserID.parse(data["FROM"])
         new_obj.group_id = data["GROUP_ID"]
-        new_obj.update_type = data["UPDATE_TYPE"]
-        new_obj.update_data = data.get("UPDATE_DATA", "")
-        new_obj.message_id = MessageID.parse(data["MESSAGE_ID"])
+        new_obj.add = data["ADD"]
+        new_obj.remove = data.get("REMOVE")
         new_obj.timestamp = Timestamp.parse(int(data["TIMESTAMP"]))
         new_obj.token = Token.parse(data["TOKEN"])
         Token.validate_token(new_obj.token, expected_scope=Token.Scope.GROUP, expected_user_id=new_obj.from_user)
@@ -49,17 +63,44 @@ class GroupUpdate(BaseMessage):
         msg_format.validate_message(new_obj.payload, new_obj.__schema__)
         return new_obj
 
-    @property
-    def payload(self) -> dict:
-        return {
-            "TYPE": self.TYPE,
-            "FROM": self.from_user,
-            "GROUP_ID": self.group_id,
-            "UPDATE_TYPE": self.update_type,
-            "UPDATE_DATA": self.update_data,
-            "MESSAGE_ID": self.message_id,
-            "TIMESTAMP": self.timestamp,
-            "TOKEN": self.token,
-        }
+
+    def send(self, socket: socket.socket, ip: str = "default", port: int = 50999, encoding: str = "utf-8"):
+        msg = msg_format.serialize_message(self.payload)
+        # Group create messages should be sent to all peers
+        for peer in client_state.get_peers():
+            #if peer != self.from_user:  # Don't send to self
+                try:
+                    # Extract IP address from UserID (format is username@ip)
+                    peer_ip = str(peer).split('@')[1]
+                    socket.sendto(msg.encode(encoding), (peer_ip, port))
+                    client_logger.debug(f"Sent group create message to peer {peer} at {peer_ip}:{port}")
+                except Exception as e:
+                    client_logger.error(f"Error sending to {peer} ({str(e)})")
+        return (ip, port)
+
+
+    @classmethod
+    def receive(cls, raw: str) -> "GroupUpdate":
+        received = cls.parse(msg_format.deserialize_message(raw))
+        # When receiving a group create message, add the group to client state
+        group_members = msg_format.string_to_list(received.members)
+        member_ids = [UserID.parse(member) for member in group_members]
+        
+        # Always store the group information in client state
+        client_state.create_group(received.group_id, received.group_name, member_ids)
+        client_logger.debug(f"Stored group in client state: {received.group_id} ({received.group_name}) with members: {member_ids}")
+        
+        return received
+
+    def info(self, verbose: bool = False) -> str:
+        if verbose:
+            return f"{self.payload}"
+        display_name = client_state.get_peer_display_name(self.from_user)
+        member_count = len(msg_format.string_to_list(self.members))
+        if display_name != "":
+            return f"{display_name} created group '{self.group_name}' with {member_count} members"
+        return f"{self.from_user} created group '{self.group_name}' with {member_count} members"
+
+
 
 __message__ = GroupUpdate
